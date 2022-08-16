e2a4ae13843fe7c0e4f90df3bd4ab721b254c448
vicenterivero2021 <valdhair@oasishoteles.com>
2022-08-15 22:26:30 -0500

    dibujar con lineas

--- canvas.js
+++ canvas.js
@@ -1,15 +1,22 @@
 let d = document.getElementById('canvas');
+let inputLineas = document.getElementById('ingresarLineas');
+inputLineas.addEventListener("change", dibujarEscribir);
 let lienzo = d.getContext('2d');
 let ancho = d.width;
 let alto = d.height;
 
-let lineas = 100;
-let countL = 0;
-let espacio = ancho/lineas;
-let colorGeneral = 'green'
-for(countL = 0;countL<lineas;countL++){
-    dibujarLineas(colorGeneral,0,espacio*countL,espacio*(countL+1),alto);
+
+function dibujarEscribir() {
+    console.log(inputLineas);
+    let lineas = inputLineas.value;
+    let countL = 0;
+    let espacio = ancho / lineas;
+    let colorGeneral = 'green'
+    for (countL = 0; countL < lineas; countL++) {
+        dibujarLineas(colorGeneral, 0, espacio * countL, espacio * (countL + 1), alto);
+    }
 }
+
 
 function dibujarLineas(color, xinicial, yinicial, xfinal, yfinal) {
     lienzo.beginPath();
